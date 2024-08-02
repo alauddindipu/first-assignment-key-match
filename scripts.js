6f92c46cc818f1c98e93a5c0ec0e865fcb2c2f40
alauddinDipu <alauddin.cucse@gmail.com>
2024-08-02 15:42:43 +0600

    3 special button done

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ let generatedVal= Math.floor(Math.random() * (maxm - minm + 1)) + minm;
 // console.log(generatedVal)
 
 document.getElementById("input-auto-filed").value =generatedVal;
+document.getElementById("messageId").innerHTML="";//clear message field
 
 });
 
@@ -18,6 +19,42 @@ const showVal=document.getElementById("show");
 function appendToShow(input){
     showVal.value +=input;
 }
+
+
+
+function removeChar(inputStr) {
+    if(inputStr.value.length == 1) {
+        inputStr.value = "";
+    } else {
+        inputStr.value = inputStr.value.substring(0, inputStr.value.length - 1);
+    }
+}
+
+
+
+//clear the field
 function clearShow(){
     showVal.value="";
+    document.getElementById("messageId").innerHTML="";//clear message field
+}
+
+//check auto generated value with typed
+
+function chekValue(){
+    let autoValue= document.getElementById("input-auto-filed").value;
+
+
+    let typedValue=document.getElementById("show").value;
+    console.log(typedValue);
+
+    if(autoValue==typedValue){
+
+        document.getElementById("messageId").innerHTML="Your Secret Key is Matched!";
+        document.getElementById("messageId").style.color="Green";
+
+    }
+    else{
+        document.getElementById("messageId").innerHTML="Your Secret Key is not Matched!";
+        document.getElementById("messageId").style.color="red";
+    }
 }
