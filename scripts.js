744b952bb37ce013411dc5340a85ef6aed11925a
alauddinDipu <alauddin.cucse@gmail.com>
2024-08-03 00:41:33 +0600

    message showing with html emoji done

--- scripts.js
+++ scripts.js
@@ -5,12 +5,10 @@ let randomId= document.getElementById("random-generate");
 randomId.addEventListener("click",function(){
     let minm = 100000;
     let maxm = 999999;
-let generatedVal= Math.floor(Math.random() * (maxm - minm + 1)) + minm;
-
-
-document.getElementById("input-auto-filed").value =generatedVal;
-document.getElementById("messageId").innerHTML="";//clear message field
-
+    let generatedVal= Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+    document.getElementById("input-auto-filed").value =generatedVal;
+    
+    document.getElementById("messageId").innerHTML="";//clear message field  
 });
 
 
@@ -23,9 +21,6 @@ function appendToShow(input){
     else{
     }
 }
-
-
-
 function removeChar(inputStr) {
     if(inputStr.value.length == 1) {
         inputStr.value = "";
@@ -45,19 +40,17 @@ function clearShow(){
 //check auto generated value with typed
 function chekValue(){
     let autoValue= document.getElementById("input-auto-filed").value;
-
-
     let typedValue=document.getElementById("show").value;
-    console.log(typedValue);
 
-    if(autoValue==typedValue){
 
-        document.getElementById("messageId").innerHTML="Your Secret Key is Matched!";
-        document.getElementById("messageId").style.color="Green";
-
+    if(autoValue==typedValue && autoValue!=""){
+        document.getElementById("messageId").innerHTML="✅ Your Secret Key is Matched!";
     }
-    else{
-        document.getElementById("messageId").innerHTML="Your Secret Key is not Matched!";
-        document.getElementById("messageId").style.color="red";
+    else if(autoValue!=""){
+        document.getElementById("messageId").innerHTML="❌ Your Secret Key is not Matched!";
+    }
+    else
+    {
+        document.getElementById("messageId").innerHTML="Press Generate Secret Key";
     }
 }
