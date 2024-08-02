aafdd123d90235139c59788a8bb994bf8d6a5f01
alauddinDipu <alauddin.cucse@gmail.com>
2024-08-02 12:43:16 +0600

    Generate Key auto done

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+
+let randomId= document.getElementById("random-generate");
+randomId.addEventListener("click",function(){
+    let minm = 100000;
+    let maxm = 999999;
+let generatedVal= Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+// console.log(generatedVal)
+
+document.getElementById("input-auto-filed").value =generatedVal;
+
+});
